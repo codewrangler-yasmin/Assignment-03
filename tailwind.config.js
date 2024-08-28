0e945e719eddf3e4c1708e6848ae9d8c753e6036
codewrangler-yasmin <mahdiaayasmin@gmail.com>
2024-08-28 13:00:40 +0600

    Updated nav, banner, about us & statistic section for large and extra large devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./public/script.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "neon-green": "#ABEF5F",
+        "neon-green-light": "rgba(171, 239, 95, 0.50)",
+        mono: "#212121",
+        accent: "#494949",
+      },
+      fontFamily: {
+        manrope: ['"Manrope", sans-serif'],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
